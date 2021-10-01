1645133333e9a909a22de409659c8dfe118eda7c
Parveen Chahal <mail.chahal@gmail.com>
2021-10-02 04:44:50 +0530

    Update domain-authenticator.sh

--- domain-authenticator.sh
+++ domain-authenticator.sh
@@ -20,8 +20,11 @@ else
   NAME="_acme-challenge.$NAME"
 fi
 
-echo $DOMAIN
-echo $NAME
+echo "CERTBOT_DOMAIN: $CERTBOT_DOMAIN"
+echo "DOMAIN: $DOMAIN"
+echo "RECORD NAME: $NAME"
+echo "CERTBOT_VALIDATION: $CERTBOT_VALIDATION"
+
 az network dns record-set txt create -g $RESOURCE_GROUP -z $DOMAIN -n $NAME --ttl 120
 az network dns record-set txt add-record -g $RESOURCE_GROUP -z $DOMAIN -n $NAME -v $CERTBOT_VALIDATION
 
