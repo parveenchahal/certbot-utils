f9bf1cb5bdf7024af78acfdeb0ff7d4bbca17370
Parveen Chahal <mail.chahal@gmail.com>
2021-10-02 04:09:10 +0530

    Create domain-authenticator.sh

---
+++ domain-authenticator.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+az login --identity -u "<user>"
+RESOURCE_GROUP="<resourceGroup>"
+
+DOMAIN=$(expr match "$CERTBOT_DOMAIN" '.*\.\(.*\..*\)')
+NAME=""
+
+if [ -z $DOMAIN ]
+then
+  DOMAIN=$CERTBOT_DOMAIN
+else
+  NAME=${CERTBOT_DOMAIN%".$DOMAIN"}
+fi
+
+if [ -z $NAME ]
+then
+  NAME="_acme-challenge"
+else
+  NAME="_acme-challenge.$NAME"
+fi
+
+echo $DOMAIN
+echo $NAME
+az network dns record-set txt create -g $RESOURCE_GROUP -z $DOMAIN -n $NAME --ttl 120
+az network dns record-set txt add-record -g $RESOURCE_GROUP -z $DOMAIN -n $NAME -v $CERTBOT_VALIDATION
+
+sleep 25
